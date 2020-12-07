20be14a6eab3669031c3145d4bebc7a0436fbaac
CamiloErnestoHoyosGalindo <41376588+CamiloErnestoHoyosGalindo@users.noreply.github.com>
2020-12-06 22:18:29 -0500

    Block visualización contraseña

--- Appkinson/Front/appkinsonFront/lib/views/Login/InputFieldLogin.dart
+++ Appkinson/Front/appkinsonFront/lib/views/Login/InputFieldLogin.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
-class InputFieldLogin extends StatelessWidget {
+class InputFieldLogin extends StatefulWidget {
+  @override
+  __FormInputFieldLogin createState() => __FormInputFieldLogin();
+
+}
+
+class __FormInputFieldLogin extends State<InputFieldLogin> {
+
+  bool _obscurePassword = true;
+  String _password;
+  String _correo;
+
+  TextEditingController emailController = new TextEditingController();
+
+  void _toggle(){
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
+
+  String prueba = "prueba";
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,6 +30,7 @@ class InputFieldLogin extends StatelessWidget {
           decoration: BoxDecoration(
               border: Border(bottom: BorderSide(color: Colors.grey[200]))),
           child: TextField(
+            controller: emailController,
             decoration: InputDecoration(
                 hintText: "Ingrese su correo eléctronico",
                 hintStyle: TextStyle(color: Colors.grey),
@@ -20,13 +41,26 @@ class InputFieldLogin extends StatelessWidget {
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
               border: Border(bottom: BorderSide(color: Colors.grey[200]))),
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: "Ingrese su contraseña",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none),
-          ),
-        ),
+              child: Row(
+                  children: <Widget>[
+                    Expanded(
+                    flex: 10,
+                    child: TextFormField(
+                    decoration: const InputDecoration(
+                    hintText: "Ingrese su contraseña",
+                    ),
+                    obscureText: _obscurePassword,
+                    ),
+                    ),
+                    Expanded(
+                    child: TextButton(
+                    onPressed: _toggle,
+                    child: new Icon(_obscurePassword ? Icons.remove_red_eye_sharp: Icons.remove_red_eye_outlined),
+                    ),
+                    ),
+                ],
+               ),
+              ),
       ],
     );
   }
--- Appkinson/Front/appkinsonFront/lib/views/Register/InputFieldRegister.dart
+++ Appkinson/Front/appkinsonFront/lib/views/Register/InputFieldRegister.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-class InputFieldRegister extends StatelessWidget {
+class InputFieldRegister extends StatefulWidget {
+
+  @override
+    _InputFieldRegister createState() => _InputFieldRegister();
+  }
+
+  class _InputFieldRegister extends State<InputFieldRegister> {
+
+    bool _obscurePassword = true;
+    bool _obscureVerPassword = true;
+    String _password;
+
+    void _toggle(){
+      setState(() {
+        _obscurePassword = !_obscurePassword;
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,7 +28,7 @@ class InputFieldRegister extends StatelessWidget {
               border: Border(bottom: BorderSide(color: Colors.grey[200]))),
           child: TextField(
             decoration: InputDecoration(
-                hintText: "Enter your email",
+                hintText: "Ingrese su correo eléctronico",
                 hintStyle: TextStyle(color: Colors.grey),
                 border: InputBorder.none),
           ),
@@ -20,23 +37,45 @@ class InputFieldRegister extends StatelessWidget {
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
               border: Border(bottom: BorderSide(color: Colors.grey[200]))),
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: "Enter your password",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 10,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Ingrese su contraseña",
+                  ),
+                  obscureText: _obscurePassword,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: _toggle,
+                  child: new Icon(_obscurePassword ? Icons.remove_red_eye_sharp: Icons.remove_red_eye_outlined),
+                ),
+              ),
+            ],
           ),
         ),
         Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
               border: Border(bottom: BorderSide(color: Colors.grey[200]))),
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: "Enter your password again",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Vuelva a Ingresar su contraseña",
+                  ),
+                  obscureText: _obscureVerPassword,
+                ),
+              ),
+            ],
           ),
+        ),
+        Container(
+
         ),
       ],
     );
