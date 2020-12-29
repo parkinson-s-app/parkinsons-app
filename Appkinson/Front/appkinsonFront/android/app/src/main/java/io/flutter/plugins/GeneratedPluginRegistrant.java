27823c1253cffc89e480521821e60510705f84e2
Ericka Torres <ericka-torres@javeriana.edu.co>
2020-12-29 18:23:02 -0500

    Formulario videos casi terminado
    Falta mejorar el scroll, mejorar formlario y mandar datos al back

--- Appkinson/Front/appkinsonFront/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ Appkinson/Front/appkinsonFront/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -19,5 +19,6 @@ public final class GeneratedPluginRegistrant {
     flutterEngine.getPlugins().add(new vn.hunghd.flutter.plugins.imagecropper.ImageCropperPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.imagepicker.ImagePickerPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.videoplayer.VideoPlayerPlugin());
   }
 }
