c7baecad39849fb8086315ccf79219923f7937a1
baru <sebasvalderrama240@outlook.com>
2021-01-03 16:55:59 -0500

    terminada pantalla de calendario con hora

--- Appkinson/Front/appkinsonFront/lib/main.dart
+++ Appkinson/Front/appkinsonFront/lib/main.dart
@@ -5,6 +5,8 @@ import 'views/HomeInitial/HomePage.dart';
 import 'views/Login/LoginPage.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'views/SymptomsForm/symptomsForm.dart';
+import 'views/Calendar/CalendarScreen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +14,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(debugShowCheckedModeBanner: false, home: symptomsForm());
-    //return new MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    //return new MaterialApp(
+    //  debugShowCheckedModeBanner: false, home: CalendarScreen());
+    return new MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
