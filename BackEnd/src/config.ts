a16a50c64022d72cbc77282c35f49949036ae74c
Leyder Vera <vera_l@javeriana.edu.co>
2021-02-12 21:59:36 -0500

    new connection

--- BackEnd/src/config.ts
+++ BackEnd/src/config.ts
@@ -1,10 +1,11 @@
 export default {
     apiPath: process.env.API_PATH || '',
-    host: process.env.HOST_DB || 'localhost',
-    user: process.env.USER_DB || 'root',
+    host: process.env.HOST_DB || 'appkinson-db.cgmd1uulv47n.us-east-2.rds.amazonaws.com',
+    user: process.env.USER_DB || 'admin',
     password: process.env.PASSWORD_DB || '',
     database: process.env.NAME_DB || 'AppKinsonDB',
     connectionLimit: process.env.CON_LIMIT || 10,
     port: process.env.PORT || '8000',
+    portDB: process.env.PORTDB || 3306,
     secretKey: process.env.SECRET_KEY || 'supersecretkey'
 };
