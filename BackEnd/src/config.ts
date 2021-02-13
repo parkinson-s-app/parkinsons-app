cf0f851bae7fe8c6c2f900ee39cc14d8ff9db172
Leyder Vera <vera_l@javeriana.edu.co>
2021-02-12 22:00:34 -0500

    forgot the pass

--- BackEnd/src/config.ts
+++ BackEnd/src/config.ts
@@ -2,7 +2,7 @@ export default {
     apiPath: process.env.API_PATH || '',
     host: process.env.HOST_DB || 'appkinson-db.cgmd1uulv47n.us-east-2.rds.amazonaws.com',
     user: process.env.USER_DB || 'admin',
-    password: process.env.PASSWORD_DB || '',
+    password: process.env.PASSWORD_DB || 'appkinson6',
     database: process.env.NAME_DB || 'AppKinsonDB',
     connectionLimit: process.env.CON_LIMIT || 10,
     port: process.env.PORT || '8000',
