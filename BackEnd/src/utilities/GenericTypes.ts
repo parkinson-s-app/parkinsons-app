fb09bca48a3f1fbcbcc45130f2542c87cb07f636
Leyder Vera <lvera4@bancodebogota.com.co>
2021-02-24 02:13:13 -0500

    updating generic  types

--- BackEnd/src/utilities/GenericTypes.ts
+++ BackEnd/src/utilities/GenericTypes.ts
@@ -1,4 +1,4 @@
 export type StringNullable = string | null | undefined;
 export type NumberNullable = number | null | undefined;
 export enum RequestType { Async, Sync }
-export enum PersonType { PATIENT = 'Patient', DOCTOR = 'Doctor', CARER = 'Carer', ADMIN = 'Admin' }
+export enum PersonType { PATIENT = 'Paciente', DOCTOR = 'Doctor', CARER = 'Cuidador', ADMIN = 'Admin' }
