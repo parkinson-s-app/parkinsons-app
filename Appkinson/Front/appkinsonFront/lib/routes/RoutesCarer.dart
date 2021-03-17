284f92f694eab9b0a22fcf9790659ddaa1968461
Ericka Torres <ericka-torres@javeriana.edu.co>
2021-03-17 00:07:42 -0500

    Interacción cuidador paciente pantalla

--- Appkinson/Front/appkinsonFront/lib/routes/RoutesCarer.dart
+++ Appkinson/Front/appkinsonFront/lib/routes/RoutesCarer.dart
@@ -1,4 +1,5 @@
 import 'package:appkinsonFront/views/HomeDifferentUsers/Carer/CarerHomePage.dart';
+import 'package:appkinsonFront/views/Relations/interactionCarerPatient.dart';
 import 'package:appkinsonFront/views/profiles/Carer/CarerProfle.dart';
 import 'package:appkinsonFront/views/profiles/Carer/profileEdition/ProfileEditionCarer.dart';
 import 'package:flutter/material.dart';
@@ -18,4 +19,15 @@ class RoutesCarer {
     Navigator.push(context,
         new MaterialPageRoute(builder: (context) => ProfileEditionCarer()));
   }
+
+  toInteractionCarerPatient(BuildContext context, int idPatient) {
+    print('patient pantalla intermedia ${idPatient.toString()}');
+    Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => InteractionCarerPatient(
+              idPatient: idPatient,
+            )));
+  }
+
 }
