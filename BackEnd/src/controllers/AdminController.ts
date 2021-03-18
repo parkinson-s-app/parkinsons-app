c0f93d5446e0a6d6784a2907037f38c0977be6c9
Leyder Vera <lvera4@bancodebogota.com.co>
2021-03-18 00:11:24 -0500

    added service delete toolbox item by id

--- BackEnd/src/controllers/AdminController.ts
+++ BackEnd/src/controllers/AdminController.ts
@@ -76,4 +76,50 @@ AdminController.post('/admin/toolbox/item', verifyToken, async (req: Request, re
     }
 });
 
+AdminController.delete('/admin/toolbox/item/:id', verifyToken, async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    debug('Delete toolbox Id: %s', id);
+    // se obtiene la autenticación para saber si el usuario está con la sesión iniciada
+    const bearerHeader = req.headers['authorization'];
+    let status;
+    if( bearerHeader !== undefined ) {
+        // se verifica el tipo de persona con la sesión iniciada
+        const type = getTypeFromToken(bearerHeader);
+        if ( type === 'Admin') {
+            try {
+                // se llama al servicio encargado de eliminar una persona por el id
+                const response = await AdminService.deleteToolboxItemById(id);
+                debug('Deletion response db: %j', response);
+                if(response) {
+                    status =  constants.HTTP_STATUS_OK;
+                    // se devuelve una respuesta afirmativa indicando que el usuario ha
+                    // sido eliminado
+                    res.status(status).send('Eliminado');
+                } else {
+                    // si no hay respuesta por parte del servicio, se devuelve una respuesta
+                    // indicando error en el servidor
+                    status =  constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                    res.status(status).send('Error');
+                }
+            } catch (error) {
+                debug('Deletion Catch Error: %s, %j', error.stack, error)
+                status =  constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+                // si ocurre algún error y no se puede ejecutar la operación, se devuelve una respuesta
+                // indicando error en el servidor
+                res.status(status).send('Error');
+            }
+        } else {
+            status =  constants.HTTP_STATUS_UNAUTHORIZED;
+            // si el usuario con la sesión iniciada no es un administrador, se indica que no está
+            // autorizado para la operación
+            res.status(status).send('Unauthorized');
+        }
+    } else {
+        status =  constants.HTTP_STATUS_UNAUTHORIZED;
+        // si la petición no tiene la autenticación, se indica que no está
+        // autorizado para la operación
+        res.status(status).send('Unauthorized');
+    }
+});
+
 export default AdminController;
