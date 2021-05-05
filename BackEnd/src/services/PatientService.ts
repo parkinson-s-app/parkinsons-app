5ae120d0ee8b81ad9cb7caf83b769203d9da7e84
Leyder Vera <lvera4@bancodebogota.com.co>
2021-05-04 21:46:23 -0500

    delete duplicate hour daily report

--- BackEnd/src/services/PatientService.ts
+++ BackEnd/src/services/PatientService.ts
@@ -650,12 +650,7 @@ export default class PatientService {
                     Hora: index,
                     Estado: result[index]
                 }
-                const item2 = {
-                    Hora: index+0.9,
-                    Estado: result[index]
-                }
                 resp.push(item);
-                resp.push(item2);
             }
             debug('response daily final :%j', resp);
             return resp;
