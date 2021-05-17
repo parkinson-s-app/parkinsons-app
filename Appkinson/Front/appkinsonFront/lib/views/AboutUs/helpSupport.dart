2795a6de490c4ab1b2bac11cce78dbbaf1bdeb8c
CamiloErnestoHoyosGalindo <41376588+CamiloErnestoHoyosGalindo@users.noreply.github.com>
2021-05-17 18:14:34 -0500

    .

--- Appkinson/Front/appkinsonFront/lib/views/AboutUs/helpSupport.dart
+++ Appkinson/Front/appkinsonFront/lib/views/AboutUs/helpSupport.dart
@@ -4,31 +4,42 @@ class HelpSupport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(''),
-        ),
-        body: SafeArea(
-            child: Container(
-                child: Column(children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              "Acerca de Nosotros",
-              style: TextStyle(fontSize: 20.0, color: Colors.blue[900]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              "Somos un grupo conformado por estudiantes y profesores expertos en " +
-                  "construccion de software y neurología. Estamos comprometidos a mejorar la " +
-                  "calidad de vida de pacientes diagnosticados con la enfermedad de " +
-                  "Parkinson. Si existe algun problema al usar esta aplicación puede " +
-                  "contactarnos por medio de este correo vera_l@javeriana.edu.co.",
-              textAlign: TextAlign.justify,
-              style: TextStyle(fontSize: 15.0, color: Colors.grey[600]),
-            ),
-          ),
-        ]))));
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Acerca de Nosotros",
+                  style: TextStyle(
+                    fontSize: 20.0, 
+                    color: Colors.blue[900]
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Somos un grupo conformado por estudiantes y profesores expertos en " +
+                      "construccion de software y neurología. Estamos comprometidos a mejorar la " +
+                      "calidad de vida de pacientes diagnosticados con la enfermedad de " +
+                      "Parkinson. Si existe algun problema al usar esta aplicación puede " +
+                      "contactarnos por medio de este correo vera_l@javeriana.edu.co.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 15.0, 
+                    color: Colors.grey[600]
+                  ),
+                ),
+              ),
+            ]
+          )
+        )
+      )
+    );
   }
 }
